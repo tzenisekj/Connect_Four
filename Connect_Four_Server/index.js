570bd65cfc907a2f6270a67c8e281ef7a0441fc5
Tyler Zenisek <tzenisekj@gmail.com>
2023-01-31 00:14:59 -0600

    updated socket interface

--- Connect_Four_Server/index.js
+++ Connect_Four_Server/index.js
@@ -1,10 +1,26 @@
 const express = require("express"); 
 const env = require("dotenv"); 
+const { Server } = require("socket.io");
+const http = require("http");
 
 env.config(); 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",  
+    }
+}); 
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server Initalized on port ${process.env.PORT}...`)
-})
+io.on("connection", (socket) => {
+    console.log(`Socket has connected... -> ${socket.id}`);
+    socket.emit("on-going games", []); 
+    socket.on("disconnect", () => {
+        console.log(`${socket.id} has disconnected...`)
+    })
+});
+
+server.listen(process.env.PORT, () => {
+    console.log(`Server Initalized on port ${process.env.PORT}...`); 
+}); 
 
