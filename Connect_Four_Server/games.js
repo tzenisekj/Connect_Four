e58ad40493f08e72e2f181f46f3df336912f6965
Tyler Zenisek <tzenisekj@gmail.com>
2023-02-02 11:02:19 -0600

    modified server

---
+++ Connect_Four_Server/games.js
@@ -0,0 +1,227 @@
+export const games = [
+    {
+        id: v4id.v4(),
+        player1: "Terminator",
+        player2: "----------",
+        spectators: 0,
+        joinable: true,
+        passwordLocked: true
+    },
+    {
+        id: v4id.v4(),
+        player1: "Tyler",
+        player2: "Red Rob",
+        spectators: 20,
+        joinable: false,
+        passwordLocked: false
+    },
+    {
+        id: v4id.v4(),
+        player1: "QSwKLegacy",
+        player2: "EnchiladaMan",
+        spectators: 1,
+        joinable: false,
+        passwordLocked: false
+    },
+    {
+        id: v4id.v4(),
+        player1: "Terminator",
+        player2: "----------",
+        spectators: 0,
+        joinable: true,
+        passwordLocked: true
+    },
+    {
+        id: v4id.v4(),
+        player1: "Terminator",
+        player2: "----------",
+        spectators: 0,
+        joinable: true,
+        passwordLocked: true
+    },
+    {
+        id: v4id.v4(),
+        player1: "Terminator",
+        player2: "----------",
+        spectators: 0,
+        joinable: true,
+        passwordLocked: true
+    },
+    {
+        id: v4id.v4(),
+        player1: "Terminator",
+        player2: "----------",
+        spectators: 0,
+        joinable: true,
+        passwordLocked: true
+    },
+    {
+        id: v4id.v4(),
+        player1: "Terminator",
+        player2: "----------",
+        spectators: 0,
+        joinable: true,
+        passwordLocked: true
+    },
+    {
+        id: v4id.v4(),
+        player1: "Terminator",
+        player2: "----------",
+        spectators: 0,
+        joinable: true,
+        passwordLocked: true
+    },
+    {
+        id: v4id.v4(),
+        player1: "Terminator",
+        player2: "----------",
+        spectators: 0,
+        joinable: true,
+        passwordLocked: true
+    },
+    {
+        id: v4id.v4(),
+        player1: "Terminator",
+        player2: "----------",
+        spectators: 0,
+        joinable: true,
+        passwordLocked: true
+    },
+    {
+        id: v4id.v4(),
+        player1: "Terminator",
+        player2: "----------",
+        spectators: 0,
+        joinable: true,
+        passwordLocked: true
+    },
+    {
+        id: v4id.v4(),
+        player1: "Terminator",
+        player2: "----------",
+        spectators: 0,
+        joinable: true,
+        passwordLocked: true
+    },
+    {
+        id: v4id.v4(),
+        player1: "Terminator",
+        player2: "----------",
+        spectators: 0,
+        joinable: true,
+        passwordLocked: true
+    },
+    {
+        id: v4id.v4(),
+        player1: "Terminator",
+        player2: "----------",
+        spectators: 0,
+        joinable: true,
+        passwordLocked: true
+    },
+    {
+        id: v4id.v4(),
+        player1: "Terminator",
+        player2: "----------",
+        spectators: 0,
+        joinable: true,
+        passwordLocked: true
+    },
+    {
+        id: v4id.v4(),
+        player1: "Terminator",
+        player2: "----------",
+        spectators: 0,
+        joinable: true,
+        passwordLocked: true
+    },
+    {
+        id: v4id.v4(),
+        player1: "Terminator",
+        player2: "----------",
+        spectators: 0,
+        joinable: true,
+        passwordLocked: true
+    },
+    {
+        id: v4id.v4(),
+        player1: "Terminator",
+        player2: "----------",
+        spectators: 0,
+        joinable: true,
+        passwordLocked: true
+    },
+    {
+        id: v4id.v4(),
+        player1: "Terminator",
+        player2: "----------",
+        spectators: 0,
+        joinable: true,
+        passwordLocked: true
+    },
+    {
+        id: v4id.v4(),
+        player1: "Terminator",
+        player2: "----------",
+        spectators: 0,
+        joinable: true,
+        passwordLocked: true
+    },
+    {
+        id: v4id.v4(),
+        player1: "Terminator",
+        player2: "----------",
+        spectators: 0,
+        joinable: true,
+        passwordLocked: true
+    },
+    {
+        id: v4id.v4(),
+        player1: "Terminator",
+        player2: "----------",
+        spectators: 0,
+        joinable: true,
+        passwordLocked: true
+    },
+    {
+        id: v4id.v4(),
+        player1: "Terminator",
+        player2: "----------",
+        spectators: 0,
+        joinable: true,
+        passwordLocked: true
+    },
+    {
+        id: v4id.v4(),
+        player1: "Terminator",
+        player2: "----------",
+        spectators: 0,
+        joinable: true,
+        passwordLocked: true
+    },
+    {
+        id: v4id.v4(),
+        player1: "Terminator",
+        player2: "----------",
+        spectators: 0,
+        joinable: true,
+        passwordLocked: true
+    },
+    {
+        id: v4id.v4(),
+        player1: "Terminator",
+        player2: "----------",
+        spectators: 0,
+        joinable: true,
+        passwordLocked: true
+    },
+    {
+        id: v4id.v4(),
+        player1: "Terminator",
+        player2: "----------",
+        spectators: 0,
+        joinable: true,
+        passwordLocked: true
+    },
+
+]
